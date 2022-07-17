e8a43e3a84829c90927428a65a62c043ce1ce790
Udofalcon <bear.arms.cavalry@gmail.com>
2022-07-17 16:18:01 -0400

    Basic jukebox data tests.

--- src/app/audio/jukebox-data/jukebox-data.spec.ts
+++ src/app/audio/jukebox-data/jukebox-data.spec.ts
@@ -5,6 +5,9 @@ describe('JukeboxData', () => {
 
   beforeEach(() => {
     jukeboxData = new JukeboxData();
+    jukeboxData.tiles.push({});
+    jukeboxData.allEdges.push({});
+    jukeboxData.deletedEdges.push({});
   });
 
   afterEach(() => {
@@ -12,6 +15,17 @@ describe('JukeboxData', () => {
   });
 
   it('should create an instance', () => {
-    expect(jukeboxData).toBeTruthy();
+    expect(jukeboxData).toBeInstanceOf(JukeboxData);
+    expect(typeof jukeboxData.maxBranches).toBe("number");
+    expect(typeof jukeboxData.maxBranchThreshold).toBe("number");
+    expect(typeof jukeboxData.currentThreshold).toBe("number");
+    expect(jukeboxData.tiles).toBeInstanceOf(Array);
+    expect(jukeboxData.tiles[0]).toBeInstanceOf(Object);
+    expect(jukeboxData.allEdges).toBeInstanceOf(Array);
+    expect(jukeboxData.allEdges[0]).toBeInstanceOf(Object);
+    expect(jukeboxData.deletedEdges).toBeInstanceOf(Array);
+    expect(jukeboxData.deletedEdges[0]).toBeInstanceOf(Object);
+    expect(typeof jukeboxData.randomBranchChanceDelta).toBe("number");
+    expect(typeof jukeboxData.lastBranchPoint).toBe("undefined");
   });
 });
